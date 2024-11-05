9079ff4fd960a76f1871251eb8b9a4a54b1b678a
LinaMVG <limarcegarzon@gmail.com>
2024-11-04 22:58:18 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './card'
+
+function App() {
+
+  return (
+  <div className='App'>
+    <h1>Carga de Estudiantes</h1>
+    <form></form>
+    <Card></Card>
+  </div>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Card =()=>{
+    return
+    <div>Hola, soy la tarjeta!</div>
+};
+
+export default Card;
